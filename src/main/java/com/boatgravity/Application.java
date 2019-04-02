c6b730c7b6005f60965afc8c7fc7446d06037e20
blakebaker <blakebaker@users.noreply.github.com>
2019-04-02 16:36:08 -0700

    Initial commit

---
+++ src/main/java/com/boatgravity/Application.java
@@ -0,0 +1,23 @@
+package com.boatgravity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@RestController(value = "/")
+	private class boatController{
+		@GetMapping
+		public String getABoat() {
+			return "BLAKE needs a boat";
+		}
+	}
+
+}
